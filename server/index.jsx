d20913d4e27c9af921ac68c03fe0d4722236b879
Mamba-cita <EDWINSJOHN52@gmail.com>
2023-07-03 13:10:32 +0300

    all details

---
+++ server/index.jsx
@@ -0,0 +1,31 @@
+const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
+require('dotenv').config();
+const { graphqlHTTP  } = require('express-graphql');
+const schema = require('./schema/schema');
+const dbConnect = require('./config/db');
+const port = process.env.PORT || 5000;
+
+
+
+
+const app = express();
+
+// db Connection
+
+dbConnect();
+
+app.use(cors());
+
+app.use('/tsm', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+
+
+
+
+
+
+app.listen(port, console.log(` TSM server running on ${port}`));
